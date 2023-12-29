1b87c979b0a6d724d56ae92f5ffc5d34f6fae323
LeviNikhil <nikhilkumarjaiswal2946@gmail.com>
2023-12-29 19:30:38 +0530

    Connected Prisma ORM With Postgresql

--- Twitter-Server/src/app/index.ts
+++ Twitter-Server/src/app/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "../clients";
 
 export async function initServer() {
   const app = express();
